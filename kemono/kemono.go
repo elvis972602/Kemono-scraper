elvis972602/Kemono-scraper

commit 71fac2274f410f63f5570fae2def29aa42a57a30

fixed issue(#25) files are skipped when there is no banner, update image format

kemono/kemono.go
@@ -258,7 +258,8 @@ func (k *Kemono) Start() {
 
 		// filter attachments
 		for i, post := range posts {
-			if k.Banner && post.File.Path != "" {
+			// download banner if banner is true or file is not image
+			if (k.Banner && post.File.Path != "") || !isImage(filepath.Ext(post.File.Name)) {
 				res := make([]File, len(post.Attachments)+1)
 				copy(res[1:], post.Attachments)
 				res[0] = post.File

kemono/fetch.go
@@ -169,12 +169,3 @@ func AddIndexToAttachments(attachments []File) []FileWithIndex {
 	}
 	return files
 }
-
-func isImage(ext string) bool {
-	switch ext {
-	case ".jpg", ".png", ".gif", ".webp", ".bmp", ".tiff", ".svg", ".ico", ".jpeg", ".jfif", ".jpe":
-		return true
-	default:
-		return false
-	}
-}

kemono/utils.go
@@ -1,0 +1,10 @@
+package kemono
+
+func isImage(ext string) bool {
+	switch ext {
+	case ".apng", ".avif", ".bmp", ".gif", ".ico", ".cur", ".jpg", ".jpeg", ".jfif", ".pjpeg", ".pjp", ".png", ".svg", ".tif", ".tiff", ".webp", ".jpe":
+		return true
+	default:
+		return false
+	}
+}

main/main.go
@@ -494,7 +494,7 @@ func parasLink(link string) (s, service, userId, postId string) {
 		log.Fatal("invalid url")
 	}
 
-	pattern := `(?i)^(?:.*\.)?(kemono|coomer)\.party$`
+	pattern := `(?i)^(?:.*\.)?(kemono|coomer)\.(?:party|su)$`
 	re := regexp.MustCompile(pattern)
 
 	matchedSubstrings := re.FindStringSubmatch(u.Host)

main/path.go
@@ -119,7 +119,7 @@ func (t *TmplCache) Execute(typ string, config *PathConfig) string {
 
 func getTyp(ext string) string {
 	switch ext {
-	case ".jpg", ".png", ".gif", ".webp", ".bmp", ".tiff", ".svg", ".ico", ".jpeg", ".jfif", ".jpe":
+	case ".apng", ".avif", ".bmp", ".gif", ".ico", ".cur", ".jpg", ".jpeg", ".jfif", ".pjpeg", ".pjp", ".png", ".svg", ".tif", ".tiff", ".webp", ".jpe":
 		return "image"
 	case ".mp4", ".webm", ".mkv", ".avi", ".mov", ".wmv", ".flv", ".f4v", ".m4v", ".rmvb", ".rm", ".3gp", ".dat", ".ts", ".mts", ".vob":
 		return "video"

commit c70c3e40aab32a2abb0be4e25d2284ddaf920065

fixed issue(#26), a logic error introduced in commit 71fac2274f410f63f5570fae2def29aa42a57a30

kemono/kemono.go
@@ -259,7 +259,7 @@ func (k *Kemono) Start() {
 		// filter attachments
 		for i, post := range posts {
 			// download banner if banner is true or file is not image
-			if (k.Banner && post.File.Path != "") || !isImage(filepath.Ext(post.File.Name)) {
+			if (k.Banner || !isImage(filepath.Ext(post.File.Name))) && post.File.Path != "" {
 				res := make([]File, len(post.Attachments)+1)
 				copy(res[1:], post.Attachments)
 				res[0] = post.File